05682407eadcf75acd1d7427f4d74a1d8763a2eb
Jamie Warburton <jamie@hexdigital.com>
2023-05-01 19:44:54 +0100

    feat: add vercel analytics

--- src/config/nuxt/plugins.ts
+++ src/config/nuxt/plugins.ts
@@ -1,5 +1,9 @@
 // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 
 export const plugins = [
+  {
+    src: '~/plugins/vercel.client.ts',
+    mode: 'client',
+  },
   '~/plugins/logger/index.ts',
 ];
